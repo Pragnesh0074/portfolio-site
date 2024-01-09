450056aa560223579c32f53b5ff8f1f7e080375f
PragneshVagadiya <86059532+Pragnesh0074@users.noreply.github.com>
2024-01-09 20:24:21 +0530

    9/1/24

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import { Link , Outlet } from "react-router-dom";
 import './Header.css';
+import logo from "../assets/logo_white.png"
 
 function Header() {
     return (
         <div className='flex flex-col md:flex-row justify-evenly pt-8 pb-2 ml-10 md:ml-0'>
-            <div className="text-white text-base font-bold font-['Fira Code']">Pragnesh Vagadiya</div>
+            <div className="text-white text-base font-bold font-['Fira Code'] flex flex-row">
+                <img src={logo} alt="" height={50} width={50} />
+                <span className="flex items-center justify-center ml-5">Code Demon</span>
+            </div>
             <div className='navLinks flex flex-row mt-5 md:mt-0' >
                 <Link to="/" className="mx-0 md:mx-2" style={{color:'white'}}>Home</Link>
                 <Link to="/work" className="mx-2" style={{color:'white'}}>Work</Link>
